969b83a2e4fa6963aa28747e467a5e77772b0526
cyl (from Travis CI) <3137650480@qq.com>
2019-03-17 01:44:25 +0000

    Deploy InitNext/InitNext.github.io to github.com/InitNext/InitNext.github.io.git:master

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 17 2019 01:37:33 GMT+0000 (Coordinated Universal Time)
+// build time:Sun Mar 17 2019 01:44:09 GMT+0000 (Coordinated Universal Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
