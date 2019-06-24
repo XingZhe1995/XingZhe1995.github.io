499e4a8a6b0c892bf78321c75e6511578addc70f
cyl (from Travis CI) <3137650480@qq.com>
2019-06-24 15:14:05 +0000

    Deploy XingZhe1995/XingZhe1995.github.io to github.com/XingZhe1995/XingZhe1995.github.io.git:master

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 22 2019 14:43:26 GMT+0000 (Coordinated Universal Time)
+// build time:Mon Jun 24 2019 15:13:50 GMT+0000 (Coordinated Universal Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
