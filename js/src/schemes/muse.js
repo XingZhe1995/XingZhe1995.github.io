f349bd168ee34a852bceee43cc855e112fec37cb
cyl (from Travis CI) <3137650480@qq.com>
2020-01-09 13:30:10 +0000

    Deploy ChenGanBin/ChenGanBin.github.io to github.com/ChenGanBin/ChenGanBin.github.io.git:master

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 31 2019 04:35:20 GMT+0000 (Coordinated Universal Time)
+// build time:Thu Jan 09 2020 13:29:54 GMT+0000 (Coordinated Universal Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
