bbad23b7edcc2419948276b4a932ea4f36b0f023
cyl (from Travis CI) <3137650480@qq.com>
2020-01-09 14:52:29 +0000

    Deploy ChenGanBin/ChenGanBin.github.io to github.com/ChenGanBin/ChenGanBin.github.io.git:master

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 09 2020 13:29:53 GMT+0000 (Coordinated Universal Time)
+// build time:Thu Jan 09 2020 14:52:10 GMT+0000 (Coordinated Universal Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
